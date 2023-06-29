33cc7508c549731cc2819f11aa46232b5b92db4e
Priya-Shivgonda <priyanka96.xworkz@gmail.com>
2023-06-29 23:34:51 +0530

    task 28

---
+++ btm/Bottle.java
@@ -0,0 +1,140 @@
+class Poster{
+String name;
+Poster(String name)
+{
+System.out.println("Poster name is: " + name);
+}
+}
+
+
+
+class PosterRunner{
+	public static void main (String[] args)
+	{
+		System.out.println("poster runner");
+		Poster p1=new Poster("color");
+		Poster p2=new Poster("size");
+		Poster p3=new Poster("height");
+		Poster p4=new Poster("posters");
+		Poster p5=new Poster("shopping");
+		Poster p6=new Poster("custom poster");
+		Poster p7=new Poster("marriage poster");
+		Poster p8=new Poster("drawnig poster");
+		Poster p9=new Poster("poster on wall");
+		Poster p10=new Poster("functions Poster");
+		Poster p11=new Poster("daidram poster");
+	}
+}
+
+
+
+
+class Gun{
+	String name;
+	Gun(String name)
+	{
+		System.out.println("Gun name is :"+name);
+	}
+}
+
+
+class GunRunner{
+	public static void main (String[] args)
+	{
+		System.out.println("gun runner main methid");
+		Gun g1=new Gun("color");
+		Gun g2=new Gun("size");
+		Gun g3=new Gun("height");
+		Gun g4=new Gun("bullet");
+		Gun g5=new Gun("shape");
+		Gun g6=new Gun("shoot");
+		Gun g7=new Gun("Rifle");
+		Gun g8=new Gun("Beretta");
+		Gun g9=new Gun("Musket");
+		Gun g10=new Gun("Heckler & Koch");
+		Gun g11=new Gun("blunderbuss");
+	}
+}
+
+
+
+
+class Boat{
+	String name;
+	Boat(String name)
+	{
+		System.out.println("Boat name is:"+ name);
+	}
+}
+
+
+ class BoatRunner{
+	public static void main(String[] args)
+	{
+		System.out.println("Boat Runner main method...");
+		Boat b1= new Boat("Amazing Grace");
+		Boat b2= new Boat("boat color");
+		Boat b3= new Boat("boat size");
+		Boat b4= new Boat("boat lenght");
+		Boat b5= new Boat("shape");
+		Boat b6= new Boat("Ride");
+		Boat b7= new Boat("WanderLust");
+		Boat b8= new Boat("Noah");
+		Boat b9= new Boat("Atlantis");
+		Boat b10= new Boat("Neptune");
+		Boat b11= new Boat("Titan");
+		
+	}
+}
+
+
+
+class Bottle{
+	Sting name;
+	Bottle(string name);
+	{
+		System.out.println("bottel constructor:"+name);
+	}
+}
+
+
+
+
+class BottleRunner{
+	public static void main (String[] args)
+	{
+		System.out.println("bottel runner  main method..");
+		Bottle b1=new Bottle("Sprite bottle");
+		Bottle b2=new Bottle("shape");
+		Bottle b3=new Bottle("size");
+		Bottle b4=new Bottle("color");
+		Bottle b5=new Bottle("water bottle");
+		Bottle b6=new Bottle("brand");
+		Bottle b7=new Bottle("companies");
+		Bottle b8=new Bottle("price");
+		Bottle b9= new Bottle("quality");
+		Bottle b10=new Bottle("styles");
+		Bottle b11=new Bottle("quantity");
+		
+	}
+}
+
+
+/*ass Fruit{
+	String name;
+	Fruit(String name)
+	{
+		System.out.println("fruit name is:"+ name);
+	}
+}
+
+
+
+class Fruit */
+	
+	
+
+
+
+
+
---
+++ btm/Fruit.java
@@ -0,0 +1,169 @@
+class Poster{
+String name;
+Poster(String name)
+{
+System.out.println("Poster name is: " + name);
+}
+}
+
+
+
+class PosterRunner{
+	public static void main (String[] args)
+	{
+		System.out.println("poster runner");
+		Poster p1=new Poster("color");
+		Poster p2=new Poster("size");
+		Poster p3=new Poster("height");
+		Poster p4=new Poster("posters");
+		Poster p5=new Poster("shopping");
+		Poster p6=new Poster("custom poster");
+		Poster p7=new Poster("marriage poster");
+		Poster p8=new Poster("drawnig poster");
+		Poster p9=new Poster("poster on wall");
+		Poster p10=new Poster("functions Poster");
+		Poster p11=new Poster("daidram poster");
+	}
+}
+
+
+
+
+class Gun{
+	String name;
+	Gun(String name)
+	{
+		System.out.println("Gun name is :"+name);
+	}
+}
+
+
+class GunRunner{
+	public static void main (String[] args)
+	{
+		System.out.println("gun runner main methid");
+		Gun g1=new Gun("color");
+		Gun g2=new Gun("size");
+		Gun g3=new Gun("height");
+		Gun g4=new Gun("bullet");
+		Gun g5=new Gun("shape");
+		Gun g6=new Gun("shoot");
+		Gun g7=new Gun("Rifle");
+		Gun g8=new Gun("Beretta");
+		Gun g9=new Gun("Musket");
+		Gun g10=new Gun("Heckler & Koch");
+		Gun g11=new Gun("blunderbuss");
+	}
+}
+
+
+
+
+class Boat{
+	String name;
+	Boat(String name)
+	{
+		System.out.println("Boat name is:"+ name);
+	}
+}
+
+
+ class BoatRunner{
+	public static void main(String[] args)
+	{
+		System.out.println("Boat Runner main method...");
+		Boat b1= new Boat("Amazing Grace");
+		Boat b2= new Boat("boat color");
+		Boat b3= new Boat("boat size");
+		Boat b4= new Boat("boat lenght");
+		Boat b5= new Boat("shape");
+		Boat b6= new Boat("Ride");
+		Boat b7= new Boat("WanderLust");
+		Boat b8= new Boat("Noah");
+		Boat b9= new Boat("Atlantis");
+		Boat b10= new Boat("Neptune");
+		Boat b11= new Boat("Titan");
+		
+	}
+}
+
+
+
+class Bottle{
+     String name;
+	Bottle(String name
+	{
+		System.out.println("bottel constructor:"+name);
+	}
+}
+
+
+
+
+class BottleRunner{
+	public static void main (String[] args)
+	{
+		System.out.println("bottel runner  main method..");
+		Bottle b1=new Bottle("Sprite bottle");
+		Bottle b2=new Bottle("shape");
+		Bottle b3=new Bottle("size");
+		Bottle b4=new Bottle("color");
+		Bottle b5=new Bottle("water bottle");
+		Bottle b6=new Bottle("brand");
+		Bottle b7=new Bottle("companies");
+		Bottle b8=new Bottle("price");
+		Bottle b9= new Bottle("quality");
+		Bottle b10=new Bottle("styles");
+		Bottle b11=new Bottle("quantity");
+		
+	}
+}
+
+
+class Fruit{
+	String name;
+	Fruit(String name)
+	{
+		System.out.println("fruit name is:"+ name);
+	}
+}
+
+
+
+class Fruit{
+	String name;
+	String color;
+	Fruit(String name, String color)
+	{
+		System.out.println(fruit name  + " is" + color);
+	}
+}
+
+
+
+class FruitRunner{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Fruit runnerr  constructor");
+		Fruit f1=new fruit("Apple,red");
+		Fruit f2=new fruit("Mango,yellow");
+		Fruit f3=new fruit("Banana,Yellow");
+		Fruit f4=new fruit("graphes,red");
+		Fruit f5=new fruit("Cherry,red");
+		Fruit f6=new fruit("Blackberry,black");
+	    Fruit f7=new fruit("Grapes,green");
+		Fruit f8=new fruit("Watermelon,red");
+		Fruit f9=new fruit("Pineapple,Yellow");
+		Fruit f10=new fruit("Strawberry,red");
+		Fruit f10=new fruit("Guava,Green");
+	}
+}
+	
+	
+	
+
+
+
+
+
