56325b1e402f7356fb88ea265c7c6ef6d82aac4b
Priya-Shivgonda <138101085+Priya-Shivgonda@users.noreply.github.com>
2023-07-10 01:10:43 +0530

    OwnerRunner.java

--- july_7/OwnerRunner.java
+++ july_7/OwnerRunner.java
@@ -8,13 +8,11 @@ class OwnerRunner{
 	  Owner ref1=new Owner("Akash",30,"Bangalore",911081055,30000);
 	  ref1.printInstanceVariables();
 	  System.out.println("updates variables");
-      ref1.name="Lokesh";	  
-      ref1.age=25;	  
-      ref1.address="Bidar";
-      ref1.phoneNumber=889754360; 
-      ref1.income=20000;  
-	  ref1.printInstanceVariables();
-	  
-	  
+          ref1.name="Lokesh";	  
+          ref1.age=25;	  
+          ref1.address="Bidar";
+          ref1.phoneNumber=889754360; 
+          ref1.income=20000;  
+	  ref1.printInstanceVariables(); 
 	}
 }
