1e1793119f503b69eae9980170bddc3368a85879
Priya-Shivgonda <138101085+Priya-Shivgonda@users.noreply.github.com>
2023-07-10 01:32:45 +0530

    calling print method

--- july_7/OwnerRunner.java
+++ july_7/OwnerRunner.java
@@ -5,7 +5,7 @@ class OwnerRunner{
 	  Owner ref = new Owner();
 	  ref.printInstanceVariables();
 	  
-	  Owner ref1=new Owner("Akash",30,"Bangalore",911081055,30000);
+	  Owner ref1=new Owner("Akash",30,"Bangalore",9110817055,30000);
 	  ref1.printInstanceVariables();
 	  System.out.println("updates variables");
           ref1.name="Lokesh";	  
